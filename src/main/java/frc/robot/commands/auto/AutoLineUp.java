f74c5649450d4c74c1bb72439f48e3fb73e3bf56
SB510 <sean.borneman@gmail.com>
2023-02-12 21:25:27 -0500

    starting arm code

--- src/main/java/frc/robot/commands/auto/AutoLineUp.java
+++ src/main/java/frc/robot/commands/auto/AutoLineUp.java
@@ -1,14 +1,23 @@
 package frc.robot.commands.auto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.NavX;
 import com.google.gson.*;
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPoint;
 public class AutoLineUp extends CommandBase {
 	public Drivetrain drivetrain;
 
@@ -26,6 +35,11 @@ public class AutoLineUp extends CommandBase {
     //static HashMap<String, GenericEntry> tagMap = new HashMap<String, GenericEntry>(); 
 
 	private JsonArray limeLightAprilTagPosition;
+	double tagX;
+	double tagY;
+	double tagZ;
+
+	List<PathPoint> toTagPath;
     
 	public AutoLineUp(Drivetrain drivetrain) {
 		this.drivetrain = drivetrain;
@@ -51,6 +65,8 @@ public class AutoLineUp extends CommandBase {
 			aprilTagID = -1;
 			aprilTagYaw = 0;
 		}
+		toTagPath = new ArrayList<PathPoint>(Arrays.asList(new PathPoint(new Translation2d(0,0), new Rotation2d(0)), new PathPoint(new Translation2d(tagX, tagZ), new Rotation2d(0))));
+		PathPlanner.generatePath(Constants.Subsystems.Drivetrain.MAX_VELOCITY_METERS_PER_SECOND, 2, path);
   	}
 
 	@Override
@@ -63,11 +79,11 @@ public class AutoLineUp extends CommandBase {
         );
 		limeLightDataArray =  (JsonArray) ((JsonObject) limeLightData.get("Results")).get("Fiducial");
 		limeLightAprilTagPosition = (JsonArray)limeLightDataArray.get(0).getAsJsonObject().get("t6t_cs");
-		tagX = limeLightAprilTagPosition.get(0); //WE acre abou this
-		tagY = taglimeLightAprilTagPosition.get(0));
-		tagZ = taglimeLightAprilTagPosition.get(0)); // WE carea bou tthis one tooo
+		tagX = limeLightAprilTagPosition.get(0).getAsDouble(); //WE acre abou this
+		tagY = limeLightAprilTagPosition.get(0).getAsDouble();
+		tagZ = limeLightAprilTagPosition.get(0).getAsDouble(); // WE carea bou this one too
 
-
+		
 		if(limeLightDataArray.size() != 0){
 			//System.out.println(limeLightDataArray.get(0).getAsJsonObject().get("fID"));
 			//System.out.println(limeLightDataArray.get(0).getAsJsonObject().get("tx"));
