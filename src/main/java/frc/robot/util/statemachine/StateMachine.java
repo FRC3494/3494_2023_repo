2aa0489747c7129ca5387deca7fb5546645a4eca
BigAlGo <allenthomasbrown@gmail.com>
2023-03-18 21:37:55 -0400

    Add some code for closest path solution

--- src/main/java/frc/robot/util/statemachine/StateMachine.java
+++ src/main/java/frc/robot/util/statemachine/StateMachine.java
@@ -6,19 +6,30 @@ import java.util.List;
 public abstract class StateMachine<T extends StateMachineState> {
     public List<StateNode<T>> nodes = new ArrayList<>();
 
-    public int currentNode = 0;
+    public List<List<boolean>> adjacencyMatrix = new ArrayList<>();
 
-    public List<Integer> queuedNodes = new ArrayList<>();
+    public int currentNode = 0;
+    public int targetNode = 0;
+
+    public StateMachine() {
+        buildTreeFromCurrent(currentNode);
+    }
 
     public void registerControllables(IStateControllable<?> ...controllables) {
 
     }
 
     public void setTarget(T target) {
-
+        for (int i = 0; i < nodes.size(); i++) {
+            if (nodes[i].equals(target)) {
+                targetNode = i;
+                break;
+            } 
+        }
     }
 
     public void update() {
+        if (currentNode == targetNode) return;
 
     }
 }
---
+++ src/main/java/frc/robot/util/statemachine/Tree.java
@@ -0,0 +1,5 @@
+package frc.robot.util.statemachine;
+
+public class Tree {
+    
+}
