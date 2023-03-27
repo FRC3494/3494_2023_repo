725b2ae6ca4d2d4faa24ffe83a9db1e119a7fec2
SB510 <sean.borneman@gmail.com>
2023-03-27 18:53:30 -0400

    reconfigured button bindings

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -11,6 +11,7 @@ public final class OI {
     static boolean leftGridMenu = false;
     static boolean middleGridMenu = false;
     static boolean rightGridMenu = false;
+    static boolean coneMode = false;
     private static EventLoop eventLoop = new EventLoop();
 
     private static XboxController primaryController = new XboxController(Constants.OI.PRIMARY_CONTROLLER_PORT);
@@ -205,102 +206,105 @@ public final class OI {
     // Joystick(Constants.OI.SECONDARY_RIGHT_CONTROLLER_PORT);
 
     public static BooleanEvent armUndo() {
-        return rightButtonBoard.button(1, eventLoop)
-                .or(rightButtonBoard.button(2, eventLoop))
-                .or(rightButtonBoard.button(3, eventLoop))
-                .or(rightButtonBoard.button(4, eventLoop));
+        return rightButtonBoard.button(3, eventLoop);
+             //   .or(rightButtonBoard.button(2, eventLoop))
+             //   .or(rightButtonBoard.button(3, eventLoop))
+             //   .or(rightButtonBoard.button(4, eventLoop));
     }
 
     public static BooleanEvent armStore() {
-        return rightButtonBoard.button(8, eventLoop);
-    }
-
-    public static BooleanEvent armGroundIntakeCone() {
-        return leftButtonBoard.button(9, eventLoop);
-    }
-
-    public static BooleanEvent armGroundIntakeCube() {
-        return leftButtonBoard.button(10, eventLoop);
-    }
-
-    public static BooleanEvent armDoubleSubstationCone() {
-        return leftButtonBoard.button(7, eventLoop);
-    }
-
-    public static BooleanEvent armDoubleSubstationCube() {
-        return leftButtonBoard.button(8, eventLoop);
-    }
-
-    public static BooleanEvent armSingleSubstation() {
-        return rightButtonBoard.button(9, eventLoop);
-    }
-
-    public static BooleanEvent armBase4Cone2() {
-        return leftButtonBoard.button(6, eventLoop);
-    }
-
-    public static BooleanEvent armBase4Cube2() {
-        return leftButtonBoard.button(3, eventLoop);
-    }
-
-    public static BooleanEvent armBase4Cone1() {
-        return leftButtonBoard.button(5, eventLoop);
-    }
-
-    public static BooleanEvent armBase4Cube1() {
-        return leftButtonBoard.button(2, eventLoop);
-    }
-
-    public static BooleanEvent armBase2Cone1() {
         return leftButtonBoard.button(4, eventLoop);
     }
 
+    public static BooleanEvent armGroundIntakeCone() {
+        return leftButtonBoard.button(7, eventLoop).and(()->coneMode);
+    }
+
+    public static BooleanEvent armGroundIntakeCube() {
+        return leftButtonBoard.button(7, eventLoop).and(()->!coneMode);
+    }
+
+    public static BooleanEvent armDoubleSubstationCone() {
+        return leftButtonBoard.button(9, eventLoop).and(()->coneMode);
+    }
+
+    public static BooleanEvent armDoubleSubstationCube() {
+        return leftButtonBoard.button(9, eventLoop).and(()->!coneMode);
+    }
+
+    public static BooleanEvent armSingleSubstation() {
+        return leftButtonBoard.button(8, eventLoop);
+    }
+
+    public static BooleanEvent armBase4Cone2() {
+        return leftButtonBoard.button(1, eventLoop).and(()->coneMode);
+    }
+
+    public static BooleanEvent armBase4Cube2() {
+        return leftButtonBoard.button(3, eventLoop).and(()->!coneMode);
+    }
+
+    public static BooleanEvent armBase4Cone1() {
+        return leftButtonBoard.button(2, eventLoop).and(()->coneMode);
+    }
+
+    public static BooleanEvent armBase4Cube1() {
+        return leftButtonBoard.button(2, eventLoop).and(()->!coneMode);
+    }
+
+    public static BooleanEvent armBase2Cone1() {
+        return leftButtonBoard.button(4, eventLoop).and(()->coneMode);
+    }
+
     public static BooleanEvent armBase2Cube1() {
-        return leftButtonBoard.button(1, eventLoop);
+        return leftButtonBoard.button(4, eventLoop).and(()->!coneMode);
     }
 
     public static BooleanEvent armBase1Hybrid() {
-        return rightButtonBoard.button(10, eventLoop);
+        return leftButtonBoard.button(3, eventLoop);
     }
 
     public static BooleanEvent forearmFineAdjustPositiveEvent() {
         // return leftButtonBoard.button(21, eventLoop);
-        return rightButtonBoard.axisGreaterThan(1, 0.1, eventLoop);
+        return leftButtonBoard.axisGreaterThan(1, 0.1, eventLoop);
     }
 
     public static BooleanEvent forearmFineAdjustNegativeEvent() {
         // return leftButtonBoard.button(20, eventLoop);
-        return rightButtonBoard.axisLessThan(1, -0.1, eventLoop);
+        return leftButtonBoard.axisLessThan(1, -0.1, eventLoop);
     }
 
     public static BooleanEvent clawIntakeCone() {
         // return leftButtonBoard.button(21, eventLoop);
-        return leftButtonBoard.axisGreaterThan(0, -0.1, eventLoop);
+        return leftButtonBoard.axisGreaterThan(0, -0.1, eventLoop).and(()->coneMode);
     }
 
     public static BooleanEvent clawIntakeCube() {
         // return leftButtonBoard.button(20, eventLoop);
-        return leftButtonBoard.axisLessThan(1, 0.1, eventLoop);
+        return leftButtonBoard.axisLessThan(0, -0.1, eventLoop).and(()->!coneMode);
     }
 
     public static BooleanEvent clawOuttakeCone() {
         // return leftButtonBoard.button(21, eventLoop);
-        return leftButtonBoard.axisGreaterThan(0, 0.1, eventLoop);
+        return leftButtonBoard.axisGreaterThan(0, 0.1, eventLoop).and(()->coneMode);
     }
 
     public static BooleanEvent clawOuttakeCube() {
         // return leftButtonBoard.button(20, eventLoop);
-        return leftButtonBoard.axisLessThan(1, -0.1, eventLoop);
+        return leftButtonBoard.axisLessThan(0, -0.1, eventLoop).and(()->!coneMode);
+    }
+    public static BooleanEvent clawIdle(){
+        return leftButtonBoard.axisGreaterThan(0, -0.1, eventLoop).and(leftButtonBoard.axisLessThan(0, 0.1, eventLoop));
     }
 
 
     public static BooleanEvent ledsIndicateCone() {
         // return leftButtonBoard.button(20, eventLoop);
-        return rightButtonBoard.button(7, eventLoop);
+        return rightButtonBoard.button(1, eventLoop);
     }
 
     public static BooleanEvent ledsIndicateCube() {
         // return leftButtonBoard.button(20, eventLoop);
-        return rightButtonBoard.button(6, eventLoop);
+        return rightButtonBoard.button(2, eventLoop);
     }
 }
