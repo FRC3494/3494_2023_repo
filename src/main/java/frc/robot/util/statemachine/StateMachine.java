fc01eb36b94226e34bb0229880e4a0ad3615077e
SB510 <sean.borneman@gmail.com>
2023-03-28 19:46:18 -0400

    leetcode easy baybeeeeeeee

--- src/main/java/frc/robot/util/statemachine/StateMachine.java
+++ src/main/java/frc/robot/util/statemachine/StateMachine.java
@@ -1,7 +1,9 @@
 package frc.robot.util.statemachine;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -157,16 +159,70 @@ public abstract class StateMachine<TState extends StateMachineState> extends Sub
 
         if (currentNode.equals(targetNode))
             return;
-        SeekResult s = new SeekResult();
-        s.sequence = find(currentNode);
+        /*
+         * SeekResult s = new SeekResult();
+         * s.sequence = find(currentNode);
+         * 
+         * SeekResult result = s;
+         * System.out.println(result.sequence);
+         * // if (result.distance >= 0) {
+         * if (result.sequence.size() > 0) {
+         * currentSequence = result.sequence;
+         * sequenceChanged = true;
+         * }
+         */
 
-        SeekResult result = s;
-        System.out.println(result.sequence);
-        // if (result.distance >= 0) {
-        if (result.sequence.size() > 0) {
-            currentSequence = result.sequence;
+        int nodesLength = nodes.size(); // number of nodes
+        int source = nodes.indexOf(currentNode); // source vertex
+
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        boolean used[] = new boolean[nodesLength];
+        int d[] = new int[nodesLength];
+        int parents[] = new int[nodesLength];
+
+        queue.push(source);
+        used[source] = true;
+        parents[source] = -1;
+        while (!queue.isEmpty()) {
+            int v = queue.pop();
+            
+            for (TState key : adjacencyMatrix.get(nodes.get(v)).keySet()) {
+                if (adjacencyMatrix.get(nodes.get(v)).get(key) < 0)
+                    continue;
+
+                int u = nodes.indexOf(key);
+
+                if (!used[u]) {
+                    used[u] = true;
+                    queue.push(u);
+                    d[u] = d[v] + 1;
+                    parents[u] = v;nodes.get(v);
+                }
+                
+            }
+        }
+
+        int targetIndex = nodes.indexOf(targetNode);
+
+        if (!used[targetIndex]) {
+            System.out.println("No path!");
+        } else {
+            ArrayList<Integer> path = new ArrayList<Integer>();
+
+            for (int v = targetIndex; v != -1; v = parents[v])
+                path.add(v);
+
+            Collections.reverse(path);
+
+            for (int v : path) {
+                if (!nodes.get(v).equals(currentNode))
+                    currentSequence.add(nodes.get(v));
+            }
+
             sequenceChanged = true;
         }
+
+        System.out.println("done!");
     }
 
     ArrayList<TState> find(TState startingNode) {
